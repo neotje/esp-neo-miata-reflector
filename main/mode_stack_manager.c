1dcf0e35650c8540cf14ee15f6cf6ed1e175778b
neotje <neotje111@gmail.com>
2024-03-07 11:54:31 +0100

    Add mode stack manager task and fix priority comparison bug

--- main/mode_stack_manager.c
+++ main/mode_stack_manager.c
@@ -1,9 +1,37 @@
 #include "mode_stack_manager.h"
 
+/*
+ * PRIVATE
+ */
+
 static const char *TAG = "ModeStackManager";
 
 enmr_mode_stack_manager_state_t *state = NULL;
 enmr_mode_stack_manager_config_t *config = NULL;
+
+TaskHandle_t enmr_mode_stack_manager_task_handle = NULL;
+
+static void enmr_mode_stack_manager_task(void *args)
+{
+    ESP_LOGI(TAG, "Mode stack manager task started");
+    while (1)
+    {
+        enmr_marker_mode_t *current_mode = enmr_mode_stack_manager_get_current_mode();
+
+        if (current_mode != NULL && current_mode->mode_function != NULL)
+        {
+            current_mode->mode_function();
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(CONFIG_MODE_LOOP_DELAY));
+    }
+}
+
+
+
+/*
+ * PUBLIC
+ */
 
 enmr_mode_stack_manager_state_t *enmr_mode_stack_manager_new_state(enmr_marker_mode_t *idle_mode)
 {
@@ -43,7 +71,7 @@ esp_err_t enmr_mode_stack_manager_init(enmr_mode_stack_manager_config_t *cnf)
     if (
         cnf == NULL ||
         cnf->idle_mode == NULL ||
-        cnf->idle_mode->config->priority != IDLE_MODE_PRIORITY)
+        cnf->idle_mode->config.priority != IDLE_MODE_PRIORITY)
     {
         return ESP_ERR_INVALID_ARG;
     }
@@ -56,6 +84,16 @@ esp_err_t enmr_mode_stack_manager_init(enmr_mode_stack_manager_config_t *cnf)
     }
 
     config = cnf;
+
+    ESP_LOGI(TAG, "Starting mode stack manager task");
+    xTaskCreate(
+        enmr_mode_stack_manager_task, 
+        "mode_stack_manager_task", 
+        2048, 
+        NULL, 
+        uxTaskPriorityGet(NULL) + CONFIG_MODE_STACK_TASK_PRIORITY, 
+        &enmr_mode_stack_manager_task_handle
+    );
 
     return ESP_OK;
 }
@@ -72,7 +110,7 @@ esp_err_t enmr_mode_stack_manager_enter(enmr_marker_mode_t *mode)
         enmr_marker_mode_t *current_mode = stack[i];
 
         if (
-            current_mode->config->priority == mode->config->priority ||
+            current_mode->config.priority == mode->config.priority ||
             stack[i] == mode)
         {
             return ESP_FAIL;
@@ -98,7 +136,7 @@ esp_err_t enmr_mode_stack_manager_enter(enmr_marker_mode_t *mode)
 esp_err_t enmr_mode_stack_manager_exit(enmr_marker_mode_t *mode)
 {
     // Cannot remove the idle mode
-    if (mode->config->priority == IDLE_MODE_PRIORITY || mode == config->idle_mode)
+    if (mode->config.priority == IDLE_MODE_PRIORITY || mode == config->idle_mode)
     {
         return ESP_ERR_INVALID_ARG;
     }
@@ -144,7 +182,7 @@ esp_err_t enmr_mode_stack_manager_sort()
     {
         for (size_t j = i + 1; j < state->stack_size; j++)
         {
-            if ((stack[i]->config->priority) < (stack[j]->config->priority))
+            if ((stack[i]->config.priority) < (stack[j]->config.priority))
             {
                 enmr_marker_mode_t *temp = stack[i];
                 stack[i] = stack[j];
--- main/mode_stack_manager.h
+++ main/mode_stack_manager.h
@@ -1,3 +1,6 @@
+#ifndef MODESTACKMANAGER_H
+#define MODESTACKMANAGER_H
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -5,10 +8,54 @@
 #include "esp_check.h"
 #include "esp_log.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "marker_mode.h"
 
-#ifndef MODESTACKMANAGER_H
-#define MODESTACKMANAGER_H
+/*
+    enmr_marker_mode_t idle_mode = MARKER_MODE_INIT_IDLE_MODE();
+
+    enmr_marker_mode_config_t daytime_mode_config = {
+        .brightness = 100,
+        .priority = 1,
+    };
+    enmr_marker_mode_t daytime_mode = {
+        .name = "Daytime Mode",
+        .config = &daytime_mode_config,
+        .state = NULL,
+    };
+
+    enmr_marker_mode_config_t blinker_mode_config = {
+        .brightness = 100,
+        .priority = 2,
+    };
+    enmr_marker_mode_t blinker_mode = {
+        .name = "Blinker Mode",
+        .config = &blinker_mode_config,
+        .state = NULL,
+    };
+
+    enmr_mode_stack_manager_config_t mode_stack_manager_config = {
+        .idle_mode = &idle_mode,
+    };
+
+    ESP_ERROR_CHECK(enmr_mode_stack_manager_init(&mode_stack_manager_config));
+
+    enmr_mode_stack_manager_print();
+
+    ESP_ERROR_CHECK(enmr_mode_stack_manager_enter(&daytime_mode));
+    enmr_mode_stack_manager_print();
+
+    ESP_ERROR_CHECK(enmr_mode_stack_manager_enter(&blinker_mode));
+    enmr_mode_stack_manager_print();
+
+    ESP_ERROR_CHECK(enmr_mode_stack_manager_exit(&daytime_mode));
+    enmr_mode_stack_manager_print();
+
+    ESP_ERROR_CHECK(enmr_mode_stack_manager_enter(&daytime_mode));
+    enmr_mode_stack_manager_print();
+*/
 
 typedef struct {
     size_t stack_size;

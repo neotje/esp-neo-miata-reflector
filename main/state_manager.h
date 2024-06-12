321adc33ecd8d4cc1e37bc45dd2410f20514ab32
Neo Hop <neotje111@gmail.com>
2024-06-12 14:20:23 +0000

    chore: Add state_manager_event.h and update devcontainer.json

--- main/state_manager.h
+++ main/state_manager.h
@@ -1,8 +1,13 @@
 #ifndef STATE_MANAGER_H
 #define STATE_MANAGER_H
 
-#include "esp_err.h"
+#include <stdint.h>
+#include <string.h>
+#include "esp_check.h"
+#include "esp_system.h"
 #include "esp_event.h"
+
+#include "state_manager_event.h"
 
 typedef struct {
     const char *key;
@@ -14,10 +19,12 @@ esp_err_t state_manager_init();
 
 esp_err_t state_manager_set(const char *key, void *value, size_t size);
 
-esp_err_t state_manager_get(const char *key, void *out, size_t* out_size);
+esp_err_t state_manager_get(const char *key, void **out, size_t* out_size);
 
 esp_err_t state_manager_register_update_handler(esp_event_handler_t event_handler, esp_event_handler_instance_t *instance);
 
 esp_err_t state_manager_unregister_update_handler(esp_event_handler_instance_t instance);
+
+void state_manager_print_entries();
 
 #endif

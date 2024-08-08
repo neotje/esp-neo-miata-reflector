e0fd08e531f3fe9752dbd9574bc95d1c97bd6e14
neotje <neotje111@gmail.com>
2024-08-08 09:27:32 +0200

    feat: Add functions for LED strip graphics functionality

--- main/gfx.h
+++ main/gfx.h
@@ -1,26 +1,70 @@
 /**
  * @file gfx.h
- * @version 1.1
+ * @version 1.0
  */
 
 #ifndef GFX_H
 #define GFX_H
 
 #include <stdint.h>
+
 #include "esp_check.h"
 #include "esp_system.h"
+#include "esp_timer.h"
 #include "led_strip.h"
+
 #include "config_manager.h"
 #include "state_manager.h"
 
+/**
+ * @brief Initialize the graphics module: create the LED strip handle, allocate memory for the output buffer and test/clear the LED strip.
+ * 
+ * @return esp_err_t 
+ */
 esp_err_t gfx_init();
 
+/**
+ * @brief Set the color of a specific pixel.
+ * 
+ * @param index index of the pixel to set
+ * @param color color to set
+ * @return esp_err_t 
+ */
 esp_err_t gfx_set(size_t index, uint32_t color);
 
+/**
+ * @brief Set the RGB color of a specific pixel.
+ * 
+ * @param index index of the pixel to set
+ * @param r red part of the color
+ * @param g green part of the color
+ * @param b blue part of the color
+ * @return esp_err_t 
+ */
 esp_err_t gfx_set_rgb(size_t index, uint8_t r, uint8_t g, uint8_t b);
 
+/**
+ * @brief Clear the LED strip.
+ * 
+ * @return esp_err_t 
+ */
 esp_err_t gfx_clear();
 
+/**
+ * @brief Start the transition of the LED strip. Save the current buffer to the previous buffer and start the transition.
+ * 
+ * @return esp_err_t 
+ */
 esp_err_t gfx_start_transition();
+
+/**
+ * @brief Linearly interpolate between two colors.
+ * 
+ * @param from 
+ * @param to 
+ * @param amount 
+ * @return uint32_t 
+ */
+uint32_t gfx_lerp_color(uint32_t from, uint32_t to, float amount);
 
 #endif
